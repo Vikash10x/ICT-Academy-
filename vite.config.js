5b9af27b39680e5d5214e576523cc0ad7795158c
Vikash Kumawat <vvikash0012@gmail.com>
2025-09-19 23:56:50 +0530

    vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
+
 export default defineConfig({
-  plugins: [require("tailwind-scrollbar-hide")],
+  plugins: [tailwindcss()],
 });
